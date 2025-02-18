ca3ad9e295a43a8ac264fc0205b8fe4347fa7f41
Zeeshan Ali Khan <zeenix@gmail.com>
2025-02-18 22:18:21 +0000

    Fix fmt

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,6 +1,10 @@
 #![no_std]
-
-#![deny(missing_debug_implementations, nonstandard_style, rust_2018_idioms, missing_docs)]
+#![deny(
+    missing_debug_implementations,
+    nonstandard_style,
+    rust_2018_idioms,
+    missing_docs
+)]
 #![warn(unreachable_pub)]
 #![doc = include_str!("../../README.md")]
 
