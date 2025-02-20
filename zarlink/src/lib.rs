8e7ba9dbecb8cda0be055e7eaaf52c9b324d520d
Zeeshan Ali Khan <zeenix@gmail.com>
2025-02-20 22:31:36 +0000

    Remove template code from `cargo new`

--- zarlink/src/lib.rs
+++ zarlink/src/lib.rs
@@ -14,19 +14,3 @@ compile_error!("Currently the `alloc` feature is required");
 pub mod connection;
 mod error;
 pub use error::{Error, Result};
-
-/// Test add.
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
