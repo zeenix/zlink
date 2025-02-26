b03b860351783c76bebaa2b4ec92dc63c39289e1
Zeeshan Ali Khan <zeenix@gmail.com>
2025-02-26 15:15:52 +0000

    More error variants

--- zarlink/src/error.rs
+++ zarlink/src/error.rs
@@ -5,7 +5,29 @@ pub enum Error {
     SocketRead,
     /// An error occurred while writing to the socket.
     SocketWrite,
+    /// Buffer overflow.
+    BufferOverflow,
+    /// Error serializing or deserializing to/from JSON.
+    #[cfg(feature = "std")]
+    Json(serde_json::Error),
+    /// Error serialization to JSON.
+    #[cfg(not(feature = "std"))]
+    JsonSerialize(serde_json_core::ser::Error),
 }
 
 /// The Result type for the zarlink crate.
 pub type Result<T> = core::result::Result<T, Error>;
+
+#[cfg(feature = "std")]
+impl From<serde_json::Error> for Error {
+    fn from(e: serde_json::Error) -> Self {
+        Error::Json(e)
+    }
+}
+
+#[cfg(not(feature = "std"))]
+impl From<serde_json_core::ser::Error> for Error {
+    fn from(e: serde_json_core::ser::Error) -> Self {
+        Error::JsonSerialize(e)
+    }
+}
