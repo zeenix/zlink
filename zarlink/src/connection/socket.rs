af23f3418352ee2d8c9628536f7a738a06d59e45
Zeeshan Ali Khan <zeenix@gmail.com>
2025-02-26 15:13:56 +0000

    Redesign `Socket` again
    Method should return `crate::Error`.

--- zarlink/src/connection/socket.rs
+++ zarlink/src/connection/socket.rs
@@ -4,14 +4,11 @@ use core::future::Future;
 ///
 /// This is the trait that needs to be implemented for a type to be used as a socket/transport.
 pub trait Socket {
-    /// The error type returned by the methods.
-    type Error: core::fmt::Display;
-
     /// Read from a socket.
     ///
     /// On completion, the number of bytes read is returned.
-    fn read(&mut self, buf: &mut [u8]) -> impl Future<Output = Result<usize, Self::Error>>;
+    fn read(&mut self, buf: &mut [u8]) -> impl Future<Output = crate::Result<usize>>;
 
     /// Write to the socket.
-    fn write(&mut self, buf: &[u8]) -> impl Future<Output = Result<(), Self::Error>>;
+    fn write(&mut self, buf: &[u8]) -> impl Future<Output = crate::Result<()>>;
 }
