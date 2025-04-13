0dc639230761435b0fa02e96ede10505df74db98
Zeeshan Ali Khan <zeenix@gmail.com>
2025-04-13 18:31:08 +0200

    🏗️ tokio: Replace async-broadcast w/ tokio API

--- zlink-tokio/src/notified.rs
+++ zlink-tokio/src/notified.rs
@@ -1,41 +1,39 @@
 //! Convenience API for maintaining state, that notifies on changes.
 
 use std::{
+    fmt::Debug,
     pin::Pin,
-    task::{Context, Poll},
+    task::{ready, Context, Poll},
 };
 
 use crate::connection::Reply;
-use async_broadcast::{Receiver, Sender};
+use tokio::sync::broadcast::{channel, Sender};
+use tokio_stream::wrappers::BroadcastStream;
 
 /// A notified state (e.g a field) of a service implementation.
 #[derive(Debug, Clone)]
 pub struct State<T, ReplyParams> {
     value: T,
     tx: Sender<ReplyParams>,
-    rx: Receiver<ReplyParams>,
 }
 
 impl<T, ReplyParams> State<T, ReplyParams>
 where
-    T: Into<ReplyParams> + Clone,
-    ReplyParams: Clone + Send + 'static,
+    T: Into<ReplyParams> + Clone + Debug,
+    ReplyParams: Clone + Send + 'static + Debug,
 {
     /// Create a new notified field.
     pub fn new(value: T) -> Self {
-        let (mut tx, rx) = async_broadcast::broadcast(1);
-        tx.set_overflow(true);
+        let (tx, _) = channel(1);
 
-        Self { value, tx, rx }
+        Self { value, tx }
     }
 
     /// Set the value of the notified field and notify all listeners.
     pub fn set(&mut self, value: T) {
         self.value = value.clone();
-        self.tx
-            .broadcast_blocking(value.into())
-            // We enabled overflow so this can't fail.
-            .unwrap();
+        // Failure means that there are currently not receivers and that's ok.
+        let _ = self.tx.send(value.into());
     }
 
     /// Get the value of the notified field.
@@ -44,29 +42,35 @@ where
     }
 
     /// Get a stream of replies for the notified field.
-    pub fn stream(&mut self) -> Stream<ReplyParams> {
-        Stream(self.rx.clone())
+    pub fn stream(&self) -> Stream<ReplyParams> {
+        Stream(self.tx.subscribe().into())
     }
 }
 
 /// The stream to use as the [`crate::Service::ReplyStream`] in service implementation when using
 /// [`State`].
 #[derive(Debug)]
-pub struct Stream<ReplyParams>(Receiver<ReplyParams>);
+pub struct Stream<ReplyParams>(BroadcastStream<ReplyParams>);
 
 impl<ReplyParams> futures_util::Stream for Stream<ReplyParams>
 where
-    ReplyParams: Clone,
+    ReplyParams: Clone + Send + 'static,
 {
     type Item = Reply<ReplyParams>;
 
-    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        Pin::new(&mut self.get_mut().0).poll_next(cx).map(|reply| {
-            reply.map(|reply| {
-                // We need to set the continues flag to true so that the client knows that this is a
-                // stream.
-                Reply::new(Some(reply)).set_continues(Some(true))
-            })
-        })
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let reply = loop {
+            match ready!(Pin::new(&mut self.0).poll_next(cx)) {
+                Some(Ok(reply)) => {
+                    break Some(Reply::new(Some(reply)).set_continues(Some(true)));
+                }
+                // Some intermediate values were missed. That's OK, as long as we get the latest
+                // value.
+                Some(Err(_)) => continue,
+                None => break None,
+            }
+        };
+
+        Poll::Ready(reply)
     }
 }
