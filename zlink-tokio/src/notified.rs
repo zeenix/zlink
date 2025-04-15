f57e3da21a506df9efbd23b4dfb8e5bc4a6195cf
Zeeshan Ali Khan <zeenix@gmail.com>
2025-04-15 10:24:55 +0200

    🚸 tokio: Add notified::Once
    This create a way to return a stream that only yields one reply.

--- zlink-tokio/src/notified.rs
+++ zlink-tokio/src/notified.rs
@@ -2,19 +2,20 @@
 
 use std::{
     fmt::Debug,
+    future::Future,
     pin::Pin,
     task::{ready, Context, Poll},
 };
 
 use crate::connection::Reply;
-use tokio::sync::broadcast::{channel, Sender};
+use tokio::sync::{broadcast, oneshot};
 use tokio_stream::wrappers::BroadcastStream;
 
 /// A notified state (e.g a field) of a service implementation.
 #[derive(Debug, Clone)]
 pub struct State<T, ReplyParams> {
     value: T,
-    tx: Sender<ReplyParams>,
+    tx: broadcast::Sender<ReplyParams>,
 }
 
 impl<T, ReplyParams> State<T, ReplyParams>
@@ -24,7 +25,7 @@ where
 {
     /// Create a new notified field.
     pub fn new(value: T) -> Self {
-        let (tx, _) = channel(1);
+        let (tx, _) = broadcast::channel(1);
 
         Self { value, tx }
     }
@@ -43,14 +44,44 @@ where
 
     /// Get a stream of replies for the notified field.
     pub fn stream(&self) -> Stream<ReplyParams> {
-        Stream(self.tx.subscribe().into())
+        Stream(StreamInner::Broadcast(self.tx.subscribe().into()))
+    }
+}
+
+/// A one-shot notified state of a service implementation.
+///
+/// This is useful for handling method calls asynchronously.
+#[derive(Debug)]
+pub struct Once<ReplyParams> {
+    tx: oneshot::Sender<ReplyParams>,
+}
+
+impl<ReplyParams> Once<ReplyParams>
+where
+    ReplyParams: Send + 'static + Debug,
+{
+    /// Create a new notified oneshot state.
+    pub fn new() -> (Self, Stream<ReplyParams>) {
+        let (tx, rx) = oneshot::channel();
+
+        (Self { tx }, Stream(StreamInner::Oneshot(rx)))
+    }
+
+    /// Set the value of the notified field and notify all listeners.
+    pub fn notify<T>(self, value: T)
+    where
+        T: Into<ReplyParams> + Debug,
+    {
+        // Failure means that we dropped the receiver stream internally before it received anything
+        // and that's a big bug that must not happen.
+        self.tx.send(value.into()).unwrap();
     }
 }
 
 /// The stream to use as the [`crate::Service::ReplyStream`] in service implementation when using
 /// [`State`].
 #[derive(Debug)]
-pub struct Stream<ReplyParams>(BroadcastStream<ReplyParams>);
+pub struct Stream<ReplyParams>(StreamInner<ReplyParams>);
 
 impl<ReplyParams> futures_util::Stream for Stream<ReplyParams>
 where
@@ -59,18 +90,39 @@ where
     type Item = Reply<ReplyParams>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        let reply = loop {
-            match ready!(Pin::new(&mut self.0).poll_next(cx)) {
-                Some(Ok(reply)) => {
-                    break Some(Reply::new(Some(reply)).set_continues(Some(true)));
-                }
-                // Some intermediate values were missed. That's OK, as long as we get the latest
-                // value.
-                Some(Err(_)) => continue,
-                None => break None,
-            }
-        };
+        match &mut self.0 {
+            StreamInner::Broadcast(stream) => {
+                let reply = loop {
+                    match ready!(Pin::new(&mut *stream).poll_next(cx)) {
+                        Some(Ok(reply)) => {
+                            break Some(Reply::new(Some(reply)).set_continues(Some(true)));
+                        }
+                        // Some intermediate values were missed. That's OK, as long as we get the
+                        // latest value.
+                        Some(Err(_)) => continue,
+                        None => break None,
+                    }
+                };
 
-        Poll::Ready(reply)
+                Poll::Ready(reply)
+            }
+            StreamInner::Oneshot(stream) => {
+                if stream.is_terminated() {
+                    return Poll::Ready(None);
+                }
+
+                Pin::new(&mut *stream).poll(cx).map(|reply| {
+                    reply
+                        .map(|reply| Reply::new(Some(reply)).set_continues(Some(false)))
+                        .ok()
+                })
+            }
+        }
     }
+}
+
+#[derive(Debug)]
+enum StreamInner<ReplyParams> {
+    Broadcast(BroadcastStream<ReplyParams>),
+    Oneshot(oneshot::Receiver<ReplyParams>),
 }
