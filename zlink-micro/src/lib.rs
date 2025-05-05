abb18b6388d2508daa3c1ff0447650df71b1ae44
Zeeshan Ali Khan <zeenix@gmail.com>
2025-05-05 23:05:40 +0200

    🎉 Skeleton for zlink-micro
    No functionality yet at all but we should publish it to grab the name
    already. 🙈

---
+++ zlink-micro/src/lib.rs
@@ -0,0 +1,13 @@
+#![deny(
+    missing_debug_implementations,
+    nonstandard_style,
+    rust_2018_idioms,
+    missing_docs
+)]
+#![warn(unreachable_pub)]
+#![doc = include_str!("../README.md")]
+
+/// Add two numbers together.
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
