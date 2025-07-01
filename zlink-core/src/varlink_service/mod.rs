144e97b1d9b7d001c8072f1f8c320875ac5dcc01
Zeeshan Ali Khan <zeenix@gmail.com>
2025-07-01 13:52:04 +0200

    🔥 core: Remove now obsolete comments

--- zlink-core/src/varlink_service/mod.rs
+++ zlink-core/src/varlink_service/mod.rs
@@ -7,14 +7,3 @@ mod info;
 pub use info::Info;
 mod error;
 pub use error::{Error, Result};
-
-// TODO: Implement introspection traits using derive macros once #[zlink(crate = "...")]
-// attribute support is working properly for types with lifetimes.
-// See: https://github.com/zeenix/zlink/issues/...
-//
-// The derive macros should generate:
-// #[cfg_attr(feature = "introspection", derive(Type))]
-// for Info<'a> struct
-//
-// #[cfg_attr(feature = "introspection", derive(ReplyError))]
-// for Error<'a> enum
